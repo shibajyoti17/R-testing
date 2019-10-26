9fdb20a7c57243853ec08dcaee06a5013cf1f9d4
unknown <shibajyotim@gmail.com>
2019-10-26 16:27:15 +0530

    4th Commit indexing and stuffs

--- Testing.R
+++ Testing.R
@@ -5,3 +5,12 @@ nrow(movielens)
 nlevels(movielens$genres)
 index <- order(murders$total)
 murders$state[index]
+print(max(murders$total))
+print(murders$state[which.max(murders$total)])
+print(rank(murders$total))
+murder_rate = (murders$total/murders$population)*100000
+print(murders$state[order(murder_rate,decreasing = TRUE)])
+safe <- murder_rate <= 1
+region <- murders$region == "West"
+index <- safe & region
+print(murders$state[index])
